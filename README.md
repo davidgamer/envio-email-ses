ab806615a4021094e7965fb062b258e94e74837d
david.silva <davidhenriquesilva.si@gmail.com>
2025-03-24 21:40:24 -0300

    adição de documentação

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Envio
+
+Api de Envio de Email (AWS e OCI)
+
+
+
+## Documentação da API
+
+http://localhost:8080/swagger-ui/index.html
+
--- src/main/java/com/email/envio/config/EmailConfig.java
+++ src/main/java/com/email/envio/config/EmailConfig.java
@@ -50,6 +50,6 @@ public class EmailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false");
     }
 }
